ed5fed963fd7f5172ec3595f887cc448bba3f4bb
robertcramer <robertbcramer@icloud.com>
2015-10-24 16:12:56 -0400

    All sections done, just need to make inline.

--- lib/etsiest.rb
+++ lib/etsiest.rb
@@ -1,5 +1,25 @@
 require "etsiest/version"
+require "sinatra/base"
+require "etsy"
+
+require "pry"
+
+Etsy.api_key = ENV['ETSY_KEY']
 
 module Etsiest
-  # Your code goes here...
+  class App < Sinatra::Base
+    set :logging, true
+
+    get "/" do
+
+      data = Etsy::Request.get('/listings/active', :includes => ['Images', 'Shop'], :keywords => 'whiskey')
+      data = data.result
+
+
+      erb :index, locals: { items: data }
+
+    end
+
+    run! if app_file == $0
+  end
 end
